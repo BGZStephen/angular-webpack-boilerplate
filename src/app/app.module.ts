acb75a8e4f789b29e4592e8b8b294160a8b239b4
Stephen Wright <sjw948@gmail.com>
2018-04-30 19:50:59 +0100

    webpack init, core angular imports

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  providers: [],
+  bootstrap: [AppComponent],
+  entryComponents: []
+})
+export class AppModule { }
